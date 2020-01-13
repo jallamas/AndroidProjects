33cf2e00d07381360f5c814e078ad9322726eeea
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2020-01-13 14:33:11 +0100

    cambios 13/01/2020

---
+++ Calculadora/app/src/main/java/org/jallamas/calculadora/MainActivity.java
@@ -0,0 +1,67 @@
+package org.jallamas.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView etScreen;
+    ImageButton btnBack;
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etScreen = findViewById(R.id.textViewScreen);
+        btn0 = findViewById(R.id.button0);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        btn5 = findViewById(R.id.button5);
+        btn6 = findViewById(R.id.button6);
+        btn7 = findViewById(R.id.button7);
+        btn8 = findViewById(R.id.button8);
+        btn9 = findViewById(R.id.button9);
+        btnBack = findViewById(R.id.buttonBack);
+
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button)v;
+        int length = etScreen.getText().toString().length();
+
+        if(b.getId()==btnBack.getId()){
+            if (length > 0) {
+                etScreen.setText(etScreen.getText().toString().substring(0,length-1));
+            }
+
+
+        } else if(etScreen.getText().toString().equals("0")){
+            etScreen.setText(b.getText().toString());
+        } else {
+            etScreen.setText(etScreen.getText().toString()+b.getText().toString());
+        }
+
+    }
+}
---
+++ VintageToyCam/app/src/main/java/org/jallamas/vintagetoycam/MainActivity.java
@@ -0,0 +1,61 @@
+package org.jallamas.vintagetoycam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView imageView;
+    Button btnNext;
+    int n=0;
+
+    List<String> imagenes= Arrays.asList("https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/La_Giralda_August_2012_Seville_Spain.jpg/240px-La_Giralda_August_2012_Seville_Spain.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Mosque_Cordoba.jpg/270px-Mosque_Cordoba.jpg",
+            "http://www.turjaen.org/sites/default/files/catedral.jpg",
+            "http://www.españaescultura.es/export/sites/cultura/multimedia/galerias/monumentos/alcazaba_almeria_t0400035.jpg_1306973099.jpg",
+            "https://cdn.civitatis.com/espana/roquetas-de-mar/galeria/alhambra.jpg",
+            "https://www.marbenjoparking.com/wp-content/uploads/2019/10/catedral-de-malaga-1080x675.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqnGDqApK1vv5uT1Zq_QKd5E3Mv-2LL7NrOX0Xxi-Bi51hEDZx&s",
+            "https://upload.wikimedia.org/wikipedia/commons/4/43/MonumentoFeDescubridora.jpg");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        btnNext = findViewById(R.id.buttonNext);
+
+        btnNext.setOnClickListener(this);
+
+        Glide
+                .with(this)
+                .load(imagenes.get(n))
+                .into(imageView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (n+1 == imagenes.size()){
+            n=0;
+        }else{
+            n++;
+        }
+        Glide
+            .with(this)
+            .load(imagenes.get(n))
+            .into(imageView);
+
+    }
+}
