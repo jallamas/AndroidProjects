e4d6392f8b174f8e9f2dde3753f86f67f6a0a4f3
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2020-01-21 14:28:44 +0100

    cambios 21/01/2020

--- Ejercicio_NasaAPODBase/app/src/main/java/android/salesianostriana/com/nasaapodbase/MainActivity.java
+++ Ejercicio_NasaAPODBase/app/src/main/java/android/salesianostriana/com/nasaapodbase/MainActivity.java
@@ -6,14 +6,20 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.salesianostriana.com.api.NasaApi;
 import android.salesianostriana.com.api.NasaPicture;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageView urlMainFoto;
+    ImageView mainFoto;
     TextView fecha,titulo,description;
 
     @Override
@@ -21,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        urlMainFoto = findViewById(R.id.imageViewMain);
+        mainFoto = findViewById(R.id.imageViewMain);
         fecha = findViewById(R.id.textViewFecha);
         titulo = findViewById(R.id.textViewTitulo);
         description = findViewById(R.id.textViewDescription);
@@ -40,20 +46,39 @@ public class MainActivity extends AppCompatActivity {
         NasaApi nasaApi = new NasaApi(apiKey);
 
         @Override
+        protected void onPreExecute(){
+            Glide
+                    .with(MainActivity.this)
+                    .load(R.drawable.ic_spinner)
+                    .centerCrop()
+                    .into(mainFoto);
+        }
+
+        @Override
         protected NasaPicture doInBackground(String... strings) {
             return nasaApi.getPicOfToday();
         }
 
         @Override
         protected void onPostExecute(NasaPicture s) {
+            //Joda Formatting
+            String rawDate = s.getDate();
+            DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
+            DateTime jodatime = dtf.parseDateTime(rawDate);
+            DateTimeFormatter dtfOut = DateTimeFormat.forPattern("dd-MM-yyyy");
+            fecha.setText(dtfOut.print(jodatime)+"\n Seleccione otra fecha");
 
-            Glide
-                    .with(MainActivity.this)
-                    .load(s.getUrl())
-                    .centerCrop()
-                    .into(urlMainFoto);
-
-            fecha.setText(s.getDate()+"Seleccione otra fecha");
+            if (s.getUrl().contains("youtube")){
+                mainFoto.setVisibility(View.GONE);
+                Toast.makeText(myActivity, "Luismi tiene el código secreto", Toast.LENGTH_LONG).show();
+            }else {
+                Glide
+                        .with(MainActivity.this)
+                        .load(s.getUrl())
+                        .error(R.drawable.ic_error)
+                        .centerCrop()
+                        .into(mainFoto);
+            }
             titulo.setText(s.getTitle());
             description.setText(s.getExplanation());
         }
